b3b1cc20ef7cfcddc351da8e64db5db153a278ba
Molnár Tímea <molnar.timi7@gmail.com>
2018-10-18 14:42:48 +0200

    Generate spring boot app with JPA+Web+MySQL

---
+++ backend/src/main/java/hu/bme/aut/szoftverarch/MAVTerkepesIncidenstar/MavTerkepesIncidenstarApplication.java
@@ -0,0 +1,12 @@
+package hu.bme.aut.szoftverarch.MAVTerkepesIncidenstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavTerkepesIncidenstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavTerkepesIncidenstarApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/hu/bme/aut/szoftverarch/MAVTerkepesIncidenstar/MavTerkepesIncidenstarApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.bme.aut.szoftverarch.MAVTerkepesIncidenstar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavTerkepesIncidenstarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
